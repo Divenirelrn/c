6e30add5369aa40543d78174b3631377c10343c9
zhaolu <zhaolu16120284@163.com>
2021-02-16 13:27:55 +0800

    添加输入输出函数

---
+++ io/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    //putchar&getchar
+//    printf("Please input character:\n");
+//    char c = getchar();
+//    printf("%c\n", c);
+    //printf&scanf
+    printf("Please input content:\n");
+    char a,b,c;
+    scanf("%c, %c, %c", &a, &b, &c);
+    printf("%c, %c, %c\n", a,b,c);
+    return 0;
+}
