474137a7d71e7ed5ac4041257dcefab7f092f7d8
zhaolu <zhaolu16120284@163.com>
2021-02-17 15:56:47 +0800

    添加注释

---
+++ pointer/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+    printf("%d", *a);
+    char s[] = "I love ai!";
+    printf("%s\n", s);
+    return 0;
+}
