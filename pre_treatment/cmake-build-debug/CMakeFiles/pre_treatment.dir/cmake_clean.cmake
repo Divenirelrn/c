1d63e9cb232f0d2ef21c6173697c65ca9c6ee427
zhaolu <zhaolu16120284@163.com>
2021-02-17 19:50:44 +0800

    添加预处理

---
+++ pre_treatment/cmake-build-debug/CMakeFiles/pre_treatment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pre_treatment.dir/main.c.obj"
+  "libpre_treatment.dll.a"
+  "pre_treatment.exe"
+  "pre_treatment.exe.manifest"
+  "pre_treatment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pre_treatment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
