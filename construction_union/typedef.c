e0565912d3e1494d7ccf0ff581f9070d1ada5440
zhaolu <zhaolu16120284@163.com>
2021-02-17 21:57:22 +0800

    添加typedef类型定义

---
+++ construction_union/typedef.c
@@ -0,0 +1,38 @@
+//
+// Created by dell on 2021/2/17.
+//
+/*
+ * 用typedef定义类型
+ *
+ * typedef声明新的类型名来代替已有的类型名（相当于起别名）
+ *
+ * typedef int INTEGER;
+ *
+ * //定义结构体类型（相当于起别名）
+ * typedef struct
+ * {
+ *   int month;
+ *   int day;
+ *   int year;
+ * }Date;
+ *
+ * typedef int NUM[100];
+ * NUM num = {0};
+ *
+ * typedef char* P;
+ * P p1;
+ *
+ * //指向函数的指针
+ * typedef int (*POINTER)();
+ * POINTER p1;
+ *
+ * typedef并没有创造新类型
+ *
+ * typedef利于程序的通用与移植
+ *
+ * 预编译
+ * 编译
+ *
+ * 
+ */
+
